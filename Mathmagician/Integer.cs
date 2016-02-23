7dba098ca1b2b32adad8f7c3b4b1e260c4cb6594
Martin Cross <mncross@gmail.com>
2016-02-22 20:23:35 -0600

    implement methods and tests for integer testing

---
+++ Mathmagician/Integer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmagician
+{
+    public class Integer
+    {
+        public int GetFirst()
+        {
+            return 0;
+        }
+        
+        public int GetNext(int current)
+        {
+            return current + 1;
+        }
+
+        public int[] GetSequence(int how_many)
+        {
+            int[] sequence_array = new int[10];
+            for (int i = 0; i < how_many; i++)
+            {
+                sequence_array[i] = i;
+            }
+            return sequence_array;
+        }
+    }
+}
